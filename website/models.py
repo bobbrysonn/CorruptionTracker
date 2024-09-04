2f2f1f828e414357010bd88ba96dc0ab8e945ea4
Bob Moriasi <official.bobmoriasi@gmail.com>
2024-09-04 12:07:10 +0300

    Added date_occured to CorruptionStory

--- website/models.py
+++ website/models.py
@@ -10,7 +10,7 @@ class CorruptionStory(models.Model):
     banner = models.ImageField(help_text="Upload the banner image")
     content = models.TextField(help_text="Enter article body")
     date_created = models.DateTimeField(auto_now_add=True)
-    date_published = models.DateTimeField(auto_now_add=True)
+    date_occured = models.DateTimeField(help_text="Enter the date the corruption occured")
     headline = models.CharField(help_text="Enter the article headline", max_length=300)
     is_published = models.BooleanField(default=False, help_text="Is published?")
 
